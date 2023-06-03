fac3ad9e799f6e2d8fdb41574220f90dd5fd4088
Harsimran jeet Singh <harsimranjeetsingh4@gmail.com>
2023-06-02 21:19:13 -0400

    Update application.py

--- application.py
+++ application.py
@@ -49,47 +49,36 @@ def scrape_google_maps(search_string, driver):
     column_names = ['Title', 'rating', 'nor', 'link']
     res_data = pd.DataFrame(columns=column_names)
 
-    # Creating loop to scrape data
-    while True:
-        # Waiting to load
-        time.sleep(3)
+    # Define loop to scroll the webpage to load all the restaurants on one page
+    k = 0
+    while k < 20:
+        temp_res = driver.find_elements("xpath", "//div[contains(@aria-label, 'Results')]/div//a[contains(@href, 'http')]")
+        action = ActionChains(driver)
+        action.move_to_element(temp_res[(len(temp_res)) - 1]).perform()
+        temp_res[(len(temp_res)) - 1].location_once_scrolled_into_view
+        k += 1
 
-        # Define loop to scroll the webpage to load all the restaurants on one page
-        k = 0
-        while k < 8:
-            temp_res = driver.find_elements("xpath", "//div[contains(@aria-label, 'Results')]/div//a[contains(@href, 'http')]")
-            action = ActionChains(driver)
-            action.move_to_element(temp_res[(len(temp_res)) - 1]).perform()
-            temp_res[(len(temp_res)) - 1].location_once_scrolled_into_view
-            k += 1
+    # Waiting to load
+    time.sleep(1)
 
-        # Waiting to load
-        time.sleep(3)
+    # Scrape result list from the results page
+    res_list = driver.find_elements(By.CLASS_NAME, 'Nv2PK.THOPZb.CpccDe')
 
-        # Scrape result list from the results page
-        res_list = driver.find_elements(By.CLASS_NAME, 'Nv2PK.THOPZb.CpccDe')
-
-        # Loop to save data of each result in a dataframe
-        for i in range(len(res_list)):
-            try:
-                # Check for the required data and save it to the dataframe.
-                # If no data available, continue to the next iteration
-                title = res_list[i].find_element(By.CSS_SELECTOR, 'div.NrDZNb').text
-                rating = res_list[i].find_element(By.CSS_SELECTOR, 'span.MW4etd').text
-                NOR = int(res_list[i].find_element(By.CSS_SELECTOR, 'span.UY7F9').text.replace("(", "").replace(")",
-                                                                                                                 "").replace(
-                    ",", ""))
-                link = res_list[i].find_element(By.CSS_SELECTOR, 'a.hfpxzc').get_attribute('href')
-                temp_data = pd.DataFrame({'Title': [title], 'rating': [rating], 'nor': [NOR], 'link': [link]})
-                res_data = pd.concat([res_data, temp_data], ignore_index=True)
-            except:
-                continue
+    # Loop to save data of each result in a dataframe
+    for i in range(len(res_list)):
         try:
-            # Click on the next page button to load the next page of results.
-            # If the button is disabled, break the loop.
-            driver.find_element(By.ID, 'ppdPk-Ej1Yeb-LgbsSe-tJiF1e').click()
+            # Check for the required data and save it to the dataframe.
+            # If no data available, continue to the next iteration
+            title = res_list[i].find_element(By.CSS_SELECTOR, 'div.NrDZNb').text
+            rating = res_list[i].find_element(By.CSS_SELECTOR, 'span.MW4etd').text
+            NOR = int(res_list[i].find_element(By.CSS_SELECTOR, 'span.UY7F9').text.replace("(", "").replace(")",
+                                                                                                             "").replace(
+                ",", ""))
+            link = res_list[i].find_element(By.CSS_SELECTOR, 'a.hfpxzc').get_attribute('href')
+            temp_data = pd.DataFrame({'Title': [title], 'rating': [rating], 'nor': [NOR], 'link': [link]})
+            res_data = pd.concat([res_data, temp_data], ignore_index=True)
         except:
-            break
+            continue
     
     # Sort the data by 'nor' (total number of reviews)
     res_data = res_data.apply(pd.to_numeric, errors='ignore')
